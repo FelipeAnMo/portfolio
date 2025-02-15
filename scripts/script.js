3c1a594129bd64e538eacff8e0cf26160dff0902
Felipe_A_Moretzsohn <126701671+FelipeAnMo@users.noreply.github.com>
2025-02-15 18:03:00 -0300

    New portfolio

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,55 @@
-$(function() {
-    $('.abilities-ul > div').on('mouseenter', function() {
-        $(this).children().css('left', '-180px');
+$(document).ready(() => {
+    $('.habilidades-item-header').on('click', function() {
+        let habilidadesBody = $(this).siblings();
+
+        if(habilidadesBody.css('height') === '0px') {
+            habilidadesBody.css({
+                'height': 'auto',
+                'margin': '10px 0px 26px 0px'
+            });
+
+            $(this).find('i').css('transform', 'rotate(-90deg)')
+        } else {
+            habilidadesBody.css({
+                'height': '0px',
+                'margin': '0px'
+            });
+
+            $(this).find('i').css('transform', 'rotate(0deg)')
+        }
     });
 
-    $('.abilities-ul > div').on('mouseleave', function() {
-        $(this).children().css('left', '0px');
+    $(".btn-inicio").on('click', () => {
+        scrollAnimation(600, 0);
+    });
+
+    $(".btn-habilidades").on('click', () => {
+        scrollAnimation(600, 850);
+    });
+
+    $(".btn-sobre").on('click', () => {
+        scrollAnimation(600, 850);
+    });
+
+    $(".btn-projetos").on('click', () => {
+        scrollAnimation(600, 1650);
+    });
+
+    $('#btn-show-mobile-header').on('click', () => {
+        $('header').toggleClass('expanded');
+    });
+
+    $('#btn-mostrar-mais').on('click', () => {
+        $('.sobre-content-mobile').html($('.sobre-content').html());
     });
 });
+
+function scrollAnimation(time, val) {
+    $("html, body").animate({
+        scrollTop: val
+    }, time);
+
+    if($(window).width() <= "580") {
+        $('header').toggleClass('expanded');
+    }
+}
